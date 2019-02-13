67fae1c3ea46d6284ec32539fedd4b90a5f6ae89
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-13 22:49:25 +0100

    Delete saikou.js

--- saikou.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'saikou',
-    description: 'Random text memes from ACG world',
-    category: 'easter',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        const saikou = require('saikou')
-message.channel.send(saikou())
-    },
-}
