93c2dcf92ce26f37138bc5c45139f46e5dfd0ed2
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-13 22:53:19 +0100

    Delete setnick.js

--- setnick.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'setnick',
-    description: 'Sets the nickname of Hitomi in the server',
-    category: 'owner',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.guild.me.setNickname(args.join(" ")).then(e=>message.channel.send("Epic")).catch(g => message.channel.send("No nickname perms 😭"))
-    },
-}
