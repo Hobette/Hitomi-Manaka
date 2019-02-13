84c04460ee148d6c7d4aa7a02944779d871df25b
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-13 22:49:45 +0100

    Delete scallop.js

--- scallop.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'scallop',
-    description: 'Shows a random quote from Pascal (most of them come from Animal Crossing: New Leaf',
-    category: 'fun',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        var quotes = utils.fs.readFileSync("./util/random/pascal.txt").toString().split("\n")
-message.channel.send(utils.random(quotes)())
-    },
-}
