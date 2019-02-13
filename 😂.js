591f21e8bb2948031ee9d7e73332e34fbddd22d2
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-13 22:40:46 +0100

    Delete 😂.js

--- 😂.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: '😂',
-    description: 'I just wanted to have a command with this name, don\'t ask',
-    category: "😂",
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.channel.send("please gimmie ideas for this command lol")
-    },
-}
