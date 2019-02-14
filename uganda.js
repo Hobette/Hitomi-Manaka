96eac6a9f4d8c4c11953820dd909e6e7aee517f3
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-14 11:27:42 +0100

    Delete uganda.js

--- uganda.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'uganda',
-    aliases: ['🇺🇬'],
-    description: 'knuckles',
-    category: 'meme',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.channel.send("🇺🇬")
-    },
-}
