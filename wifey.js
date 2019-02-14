cc2ef9f2ce469f62b6aba470f5a622912ea0ad69
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-14 11:31:34 +0100

    Delete wifey.js

--- wifey.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'wifey',
-    description: '',
-    category: 'meme',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.channel.send("老婆SouljaBoyTellEm")
-    },
-}
