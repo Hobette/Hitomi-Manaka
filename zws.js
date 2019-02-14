e6f2e688e6d02679e7f4580a8e1d8a2db7e9cd6e
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-14 11:32:09 +0100

    Delete zws.js

--- zws.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'zws',
-    description: 'Sends a zero-width space that you can copy and use',
-    category: 'pppp',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.channel.send("\u200B")
-    },
-}
