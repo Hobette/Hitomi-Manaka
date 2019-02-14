6dd742062b5e01be02a22020ff80c5c15871174b
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-14 11:23:55 +0100

    Delete trap.js

--- trap.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'trap',
-    description: '',
-    category: 'meme',
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.channel.send("Bad Bunny sucks.")
-    },
-}
