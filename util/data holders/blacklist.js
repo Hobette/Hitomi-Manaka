e48c5982f7199a1e362a498eb5ce2c7520e813a7
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-02-15 12:03:39 +0100

    Censored blacklisted users

--- util/data holders/blacklist.js
+++ util/data holders/blacklist.js
@@ -1,15 +1,9 @@
 module.exports = {
     users: [
-        "301108371057016832",   //Afkinz#8998
-        "381278153957441536",   //God Horseman#1818 (he usually changes his name, they usually have horseman or dark on it)
-        "289151878560743427",   //Hal9000#3201
-        "460713047019880448",   //commShit#9158 (owner of a BIG bot farm, tried to add hitomi twice)
-        "321693898189570059"    //Negaren#6622 (Basically, immature
+        "USER ID"
     ],
     guilds: [
-        '178313653177548800',   //notsoserver (obvious reasons)
-	'220275955329925120',   //Official SiIvagunner Fan Server (Long story)
-	'440001452077744129',   //Shibu wants to accept you (big bot farm)
-        '446359191892918295'    //Don't know the name, but was owned by 321693898189570059. Same reason for blacklisting him
+        'SERVER ID'
     ]
 }
+//Kept as a js file and not as a json because of the inability to write comments in json
