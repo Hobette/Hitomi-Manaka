5321761332233f691bb82972567d6b4bcce1a8eb
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-03-31 14:23:03 +0200

    Merged various commands here

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,15 @@
 module.exports = {
     name: 'test',
-    description: 'Command template',
+    aliases: ['commands', 'jeff', 'larious', 'tomi'],
+    description: 'Acts different depending on the alias used, to avoid making too many commands',
     category: "h",
     execute: async (client, config, Discord, target, utils, message, args) => {
+        var cmd = utils.checkCommand(message.content, "name")
+        if (cmd === "commands") return message.channel.send("Beep boop wrong command use `hi!help` instead")
+        if (cmd === "jeff") return message.channel.send("jeff")
+	if (cmd === "larious" && utils.checkCommand(message.content, "prefix") === "hi!") return message.channel.send('Nice pun')
+        if (cmd === "tomi" && utils.checkCommand(message.content, "prefix") === "hi!") return message.channel.send('Lol')
+
         var e = await message.channel.send('h')
         e.edit("If you see this, async is working")
     }
