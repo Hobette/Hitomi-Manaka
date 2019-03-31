74806fdc9b3ff671c547f4ca3a4bc60ea7c38ce8
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-03-31 14:24:50 +0200

    Command was merged with hi!test

--- commands/commands.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'commands',
-    description: 'Tells you to use the right command',
-    category: "e",
-    execute: async (client, config, Discord, target, utils, message, args) => {
-        message.channel.send('Beep boop wrong command use `hi!help` instead')
-    },
-}
