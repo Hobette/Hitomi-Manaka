d3709f0817d38de4c0df05fcba08900d524711c4
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-07-24 12:23:13 +0200

    Removed the utils.yesNo function

--- commands/faq.js
+++ commands/faq.js
@@ -3,6 +3,6 @@ module.exports = {
   description: 'fucking annoying questions',
   category: "general",
   execute: async (client, config, Discord, target, utils, message, args) => {
-       message.channel.send(`Please refer to ${utils.yesNo(message.guild.id === "431910179991322624", "<#521317263048179712>", "the #faq channel from my support server")}, thanks!`)
+       message.channel.send(`Please refer to $message.guild.id === "431910179991322624" ? "<#521317263048179712>" : "the #faq channel from my support server")}, thanks!`)
   },
 }
