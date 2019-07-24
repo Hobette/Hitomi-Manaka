fed41eb47f4af8e5e0436b3754dbdc774efad80b
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-07-24 12:38:26 +0200

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   name: 'user',
   description: 'Sees an user\'s info',
@@ -5,30 +6,30 @@ module.exports = {
   usage: '(mention or user ID or none to see your info)',
   execute: async (client, config, Discord, target, utils, message, args) => {
 
-      let sharedServers = 0;
-      for (i = 0; i < client.guilds.size; i++) {
-        if (client.guilds.array()[i].members.find(m => m.user.id === target.id) != undefined) {
-          sharedServers++;
-        }
+      var statuses = {
+        online: "green_",
+        idle: "yellow_",
+        dnd: "",
+        offline: "black_"
       }
-      if (target == client.user) { sharedServers = "America" } else { sharedServers = sharedServers + ' guilds' }
+      var gaem = "A boring game."
+      if (target.presence.game !== null) {gaem = target.presence.game.name}
 
-      var idgame 
-      if (target.presence.game != null) { idgame = target.presence.game.name } else { idgame = "Nothing -(" }
       const embed = new Discord.RichEmbed()
         .setColor(12961221)
-        .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`)
+        .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}${message.guild.members.has(target.id) ? " | Use hi!guilduser to see info about them as a member of this guild." : ""}`)
         .setThumbnail(`${target.displayAvatarURL.replace(/\?size=[0-9]+/g, "")}?size=2048`)
         .setAuthor(`User info`)
 
-        .addField("Name", `${target.username}#${target.discriminator}`, true)
-        .addField("ID", `${target.id}`, true)
-        .addField("Bot?", `${utils.yesNo(target.bot, "<:check:314349398811475968>", "<:xmark:314349398824058880>")}`, true)
-        .addField("Playing...", `${idgame}`, true)
-        .addField("Status", `${utils.firstLetterUppercase(target.presence.status)}`, true)
-        .addField("Seen on", `${sharedServers}`, true)
+        .addField("Name", target.tag, true)
+        .addField("ID", target.id, true)
+        .addField("Bot?", (target.bot ? "<:check:314349398811475968>": "<:xmark:314349398824058880>"), true)
+        .addField("Playing...", gaem, true)
+        .addField("Status", `:${statuses[target.presence.status]}heart:`, true)
+        .addField("Seen on", (target == client.user || client.guilds.filter(e=>e.members.has(target.id))).size<0 ? "The school infirmary": `${client.guilds.filter(e=>e.members.has(target.id)).size} servers.`, true)
         .addField("Created at", `${target.createdAt}`, true)
-      message.channel.send({ embed });
+
+      message.channel.send({ embed }).catch(()=> message.channel.send("I need embed permissions for this!"));
     
   },
 }
