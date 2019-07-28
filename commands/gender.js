5a25fb0f6227e26059a60014cb0c269e4e86e29e
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-07-28 21:29:41 +0200

    Changed the way it works a bit

--- commands/gender.js
+++ commands/gender.js
@@ -5,17 +5,18 @@ module.exports = {
     usage: "[optional: number of gender]",
     category: "fun",
     execute: async (client, config, Discord, target, utils, message, args) => {
-        var genders = require( "../util/random/genders.json")
-        
+        var genders = require("../util/random/genders.json")
+
         if (!args[0]) {
             var gender = utils.random(genders)()
             const embed = new Discord.RichEmbed()
-            .setDescription(`<:prideFlag:500255005266280448> **#${genders.indexOf(gender)}** ${gender}`)
+                .setDescription(`<:prideFlag:500255005266280448> **#${genders.indexOf(gender)} ${gender.replace("-", ":** ")}.`)
             message.channel.send(embed)
         } else {
-            if (!genders[args[0]]) return message.channel.send("There's no sexuality with that number!")
+            if (!genders[args[0] - 1]) return message.channel.send("There's no sexuality with that number!")
             const embed = new Discord.RichEmbed()
-                .setDescription(`<:prideFlag:500255005266280448> **#${args[0]}** ${genders[args[0]]}`)
-                message.channel.send(embed)    }
+                .setDescription(`<:prideFlag:500255005266280448> **#${args[0]} ${genders[args[0] - 1].replace("-", ":** ")}.`)
+            message.channel.send(embed)
+        }
     },
 }
