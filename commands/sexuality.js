ef47ad54e237fd697da9f581b51fe31b90e15da7
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-07-28 21:30:33 +0200

    Changed the way it works a bit

--- commands/sexuality.js
+++ commands/sexuality.js
@@ -8,12 +8,14 @@ module.exports = {
         if (!args[0]) {
         var sexuality = utils.random(sexualities)()
             const embed = new Discord.RichEmbed()
-            .setDescription(`<:prideFlag:500255005266280448> **#${sexualities.indexOf(sexuality)}** ${sexuality}`)
-            message.channel.send(embed)}
+            .setDescription(`<:prideFlag:500255005266280448> **#${sexualities.indexOf(sexuality)} ${sexuality.replace("-", ":** ")}.`)
+            message.channel.send(embed).catch(()=>message.channel.send("I need embed permissions!"))
+}
             else {
-        if (!sexualities[args[0]]) return message.channel.send("There's no sexuality with that number!")
+        if (!sexualities[args[0]-1]) return message.channel.send("There's no sexuality with that number!")
         const embed = new Discord.RichEmbed()
-            .setDescription(`<:prideFlag:500255005266280448> **#${args[0]}]}** ${sexualities[args[0]]}`)
-            message.channel.send(embed)    }
+            .setDescription(`<:prideFlag:500255005266280448> **#${args[0]} ${sexualities[args[0]-1].replace("-", ":** ")}.`)
+            message.channel.send(embed).catch(()=>message.channel.send("I need embed permissions!"))
+	    }
     },
 }
