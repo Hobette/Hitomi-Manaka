0ce31cf58cadf844af181eea1c6cd550126f8289
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-21 13:24:55 +0200

    Now it requires a txt file instead of a json file

--- commands/hitomi.js
+++ commands/hitomi.js
@@ -1,25 +1,26 @@
 module.exports = {
     name: 'hitomi',
-    description: 'Sends a random SFW pic of Hitomi, the character this bot is based off (some pics might be A LITTLE explicit. However, this command is supposed to be totally SFW)',
+    description: 'Sends a random picture of Hitomi, the character this bot is inspired on',
     category: "fun",
-    usage: "[optional: number of gepicturender]",
+    usage: "[optional: number of picture]",
     execute: async (client, config, Discord, target, utils, message, args) => {
-        var pics = require( "../util/random/hitomi.json")
+        var pics = utils.fs.readFileSync("../util/random/hitomi.txt").toString().split("\n")
         var emoj = utils.random(client.guilds.get("431910179991322624").emojis.filter(e => e.name.toLowerCase().includes("hitomi")).array())().toString()
         if (!args[0]) {
             var sendpic = utils.random(pics)()
 
             const embed = new Discord.RichEmbed()
-            .setDescription(`**#${pics.indexOf(sendpic)}** `+emoj)
+            .setDescription(`**#${pics.indexOf(sendpic)+1}** `+emoj)
             .setImage(sendpic)
-            message.channel.send(embed)
+            message.channel.send(embed).catch(()=>message.channel.send("I need embed permissions!"))
         } else {
-            if (!pics[args[0]]) return message.channel.send("There's no pic with that number!")
+            if (!pics[args[0]-1]) return message.channel.send("There's no pic with that number!")
 
             const embed = new Discord.RichEmbed()     
             .setDescription(`**#${args[0]}** `+emoj)
-            .setImage(pics[args[0]])
-            message.channel.send(embed)
+            .setImage(pics[args[0]-1])
+            message.channel.send(embed).catch(()=>message.channel.send("I need embed permissions!"))
+
         }
     },
 }
