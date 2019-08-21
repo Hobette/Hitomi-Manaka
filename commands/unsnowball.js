e37eaa1444868ab42cffbc182bfb0f80713671f8
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-21 13:41:24 +0200

    Create unsnowball.js

---
+++ commands/unsnowball.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'unsnowball',
+    description: 'Removes the `Snowball` role from you (hi!snowball)',
+    category: 'fun',
+    execute: async (client, config, Discord, target, utils, message, args) => {
+
+        if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("I need Manage Roles permissions!")
+
+        var snowball = message.guild.roles.find(m => m.name === 'Snowball')
+        if (snowball == undefined) return message.channel.send("Why would I remove a role that doesn't exist?")
+
+        if (!message.member.roles.has(snowball)) return message.channel.send("But you don¡t have the snowball role")
+
+        message.member.removeRoles([snowball])
+        .then(() => { message.channel.send("doned") })
+        .catch(() => { message.channel.send("Failed to unsnowball you!") })
+    },
+}
