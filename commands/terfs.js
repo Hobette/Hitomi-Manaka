9bef6d2bff9431f8852d9e2ac76e56201917d7cd
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-21 13:50:15 +0200

    Create terfs.js

---
+++ commands/terfs.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'terfs',
+    description: 'Be like',
+    category: 'meme',
+    execute: async (client, config, Discord, target, utils, message, args) => {
+        var viode = new Discord.Attachment("https://cdn.discordapp.com/attachments/406497072939008001/610430331161673728/videoReady.mp4", "how_yall_mfs_look_when_you_dont_respect_trans_people.mp4")
+message.channel.send(viode)
+    },
+}
