f36cdabcd70d1793ab4e229867260d6b912e20cd
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-25 21:15:48 +0200

    Fixed checking for individual users

--- commands/baltop.js
+++ commands/baltop.js
@@ -10,31 +10,31 @@ module.exports = {
             limit: 10
         })
 
-        var first = client.users.get(users[0].userid) 
-        var second = client.users.get(users[1].userid)
-        var third = client.users.get(users[2].userid) 
-        var fourth = client.users.get(users[3].userid) 
-        var fifth = client.users.get(users[4].userid) 
-        var sixth = client.users.get(users[5].userid)
-        var seventh = client.users.get(users[6].userid) 
-        var eighth = client.users.get(users[7].userid) 
-        var ninth = client.users.get(users[8].userid) 
-        var tenth = client.users.get(users[9].userid) 
+        var first = await client.fetchUser(users[0].userid) 
+        var second = await client.fetchUser(users[1].userid)
+        var third = await client.fetchUser(users[2].userid) 
+        var fourth = await client.fetchUser(users[3].userid) 
+        var fifth = await client.fetchUser(users[4].userid) 
+        var sixth = await client.fetchUser(users[5].userid)
+        var seventh = await client.fetchUser(users[6].userid) 
+        var eighth = await client.fetchUser(users[7].userid) 
+        var ninth = await client.fetchUser(users[8].userid) 
+        var tenth = await client.fetchUser(users[9].userid) 
 
             const embed = new Discord.RichEmbed()
                 .setTitle("Top 10 users")
                 .setColor("#8f91e1")
                 .setDescription(`
-[\`1\`] **${users[0].balance}<:sapphire:525655228444573696>** ~ ${first.tag}
-[\`2\`] **${users[1].balance}<:sapphire:525655228444573696>** ~ ${second.tag}
-[\`3\`] **${users[2].balance}<:sapphire:525655228444573696>** ~ ${third.tag}
-[\`4\`] **${users[3].balance}<:sapphire:525655228444573696>** ~ ${fourth.tag}
-[\`5\`] **${users[4].balance}<:sapphire:525655228444573696>** ~ ${fifth.tag}
-[\`6\`] **${users[5].balance}<:sapphire:525655228444573696>** ~ ${sixth.tag}
-[\`7\`] **${users[6].balance}<:sapphire:525655228444573696>** ~ ${seventh.tag}
-[\`8\`] **${users[7].balance}<:sapphire:525655228444573696>** ~ ${eighth.tag}
-[\`9\`] **${users[8].balance}<:sapphire:525655228444573696>** ~ ${ninth.tag}
-[\`💩\`] **${users[9].balance}<:sapphire:525655228444573696>** ~ ${tenth.tag}`)
+[\`1\`] **${users[0].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(first.id).tag}
+[\`2\`] **${users[1].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(second.id).tag}
+[\`3\`] **${users[2].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(third.id).tag}
+[\`4\`] **${users[3].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(fourth.id).tag}
+[\`5\`] **${users[4].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(fifth.id).tag}
+[\`6\`] **${users[5].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(sixth.id).tag}
+[\`7\`] **${users[6].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(seventh.id).tag}
+[\`8\`] **${users[7].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(eighth.id).tag}
+[\`9\`] **${users[8].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(ninth.id).tag}
+[\`💩\`] **${users[9].balance}<:sapphire:525655228444573696>** ~ ${client.users.get(tenth.id).tag}`)
                 message.channel.send(embed)
         } else {
             if (target == message.author) {
@@ -45,7 +45,7 @@ module.exports = {
                   })
             } else {
                 utils.eco.Leaderboard({
-                    search: client.users.get(args[0].re(/[^0-9]/g, ""))
+                    search: client.fetchUser(args[0].replace(/[^0-9]/g, ""))
                   }).then(output => {
                   message.channel.send(`The user \`${message.mentions.users.first().tag}\` is number **${output}** on my leaderboard!`);
                   })
