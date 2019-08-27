5c7d792a6fd7c5e0764ece0e7fbc3d08a9030187
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-27 13:34:10 +0200

    Several fixes

--- commands/snowball.js
+++ commands/snowball.js
@@ -1,3 +1,4 @@
+
 module.exports = {
     name: 'snowball',
     description: 'Throws a snowball at someone and gives them a `snowball role (only if there\'s any). Fails a lot if they already have it',
@@ -8,12 +9,14 @@ module.exports = {
 
         var snowball = message.guild.roles.find(m => m.name === 'Snowball')
         if (snowball == undefined) return message.channel.send("This server needs a role called `Snowball` to use this! (case sensitive)") 
-        if (!message.member.roles.has(snowball)) return message.channel.send("You need the `Snowball` role to throw snowballs!")
+        if (!message.member.roles.has(snowball.id)) return message.channel.send("You need the `Snowball` role to throw snowballs!")
 
         if (!args[0]) return message.channel.send("You need to throw it to an user that is here, nerd") 
             args[0] = args[0].replace(/[^0-9]/g, "")
 
-        if (victim == undefined) return message.channel.send("You need to throw it to an user that is here, nerd") 
+        var victim = message.guild.members.get(target.id)
+
+        if (victim  == undefined) return message.channel.send("You need to throw it to an user that is here, nerd") 
         if (victim.user.id === message.author.id || victim.user.bot) return message.channel.send("No.")
 
         message.channel.send(`${message.author} threw a snowball to ${victim}!`)
