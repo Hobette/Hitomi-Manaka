1c221c0b3144624f9c6629633ca3000ef1c7b57d
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-28 23:00:16 +0200

    description change+actually adds the role+more
    The role name isn't case sensitive anymore and I also fixed some liiitle mistake in text

--- commands/snowball.js
+++ commands/snowball.js
@@ -1,14 +1,14 @@
 
 module.exports = {
     name: 'snowball',
-    description: 'Throws a snowball at someone and gives them a `snowball role (only if there\'s any). Fails a lot if they already have it',
+    description: 'This is a game where if you have a role called `snowball` you can use this command to throw snowballs to people, and they\'ll get the role too',
     usage: "(mention or id, must be on the same server)",
     category: "fun",
     execute: async (client, config, Discord, target, utils, message, args) => {
         if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("I need Manage Roles permissions!")
 
-        var snowball = message.guild.roles.find(m => m.name === 'Snowball')
-        if (snowball == undefined) return message.channel.send("This server needs a role called `Snowball` to use this! (case sensitive)") 
+        var snowball = message.guild.roles.find(m => m.name.toLowerCase() === 'snowball')
+        if (snowball == undefined) return message.channel.send("This server needs a role called `Snowball` to use this!")
         if (!message.member.roles.has(snowball.id)) return message.channel.send("You need the `Snowball` role to throw snowballs!")
 
         if (!args[0]) return message.channel.send("You need to throw it to an user that is here, nerd") 
@@ -19,6 +19,7 @@ module.exports = {
         if (victim  == undefined) return message.channel.send("You need to throw it to an user that is here, nerd") 
         if (victim.user.id === message.author.id || victim.user.bot) return message.channel.send("No.")
 
-        message.channel.send(`${message.author} threw a snowball to ${victim}!`)
+        victim.addRoles([snowball.id])
+        message.channel.send(`${message.author} threw a snowball at ${victim}!`)
     },
 }
