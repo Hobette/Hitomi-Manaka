06f1ddabd96fdce56b3e1fbcbee036ecb8c54a01
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-08-28 23:01:03 +0200

    Now actually works+Role name is not case sensitive

--- commands/unsnowball.js
+++ commands/unsnowball.js
@@ -6,12 +6,12 @@ module.exports = {
 
         if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("I need Manage Roles permissions!")
 
-        var snowball = message.guild.roles.find(m => m.name === 'Snowball')
+        var snowball = message.guild.roles.find(m => m.name.toLowerCase() === 'Snowball')
         if (snowball == undefined) return message.channel.send("Why would I remove a role that doesn't exist?")
 
-        if (!message.member.roles.has(snowball)) return message.channel.send("But you don¡t have the snowball role")
+        if (!message.member.roles.has(snowball.id)) return message.channel.send("But you don't have the snowball role")
 
-        message.member.removeRoles([snowball])
+        message.member.removeRoles([snowball.id])
         .then(() => { message.channel.send("doned") })
         .catch(() => { message.channel.send("Failed to unsnowball you!") })
     },
