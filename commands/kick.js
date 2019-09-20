6b27032881c10e1a5ac596bada7c4c8e6746bf49
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-09-20 20:32:42 +0200

    Does not rely on utils.checkCommand anymore

--- commands/kick.js
+++ commands/kick.js
@@ -4,15 +4,13 @@ module.exports = {
     usage: "(one or multiple mentions/user IDs) (optional reason)",
     category: "mod",
     execute: async (client, config, Discord, target, utils, message, args) => {
-        
-	args.push("")
+
 
         if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Have you considered having kick perms first?")
 
-        var victims = args.map(a => a.replace(/[^0-9]+/g, ""))
-        victims = victims.slice(0, victims.indexOf(""))
-        var reason = args.slice(victims.length, args.length)
-        if (!reason[0]) { reason = `[${message.author.tag}] No reason apparently??????` } else { reason = `[${message.author.tag}] ${reason.join(" ")}` }
+        var victims = message.context.victims
+        var reason = message.context.reason
+        if (!reason[0]) { reason = `[${message.author.tag}] No reason apparently??????` } else { reason = `[${message.author.tag}] ${reason}` }
         if (!victims[0]) return message.channel.send(`That's not how you use the command!
 \`\`\`hi!kick [user 1] [optional reason]
 hi!kick [user 1] [user 2] [etc...] [optional reason]\`\`\`
