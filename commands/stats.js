f6012ae89a51a0347b6b94bfbdeec89058b55623
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-09-20 20:30:33 +0200

    Removed the cpu thing
    The reason fro this is that it takes the whole server's processes, not only hitomi's

--- commands/stats.js
+++ commands/stats.js
@@ -3,12 +3,6 @@ module.exports = {
     description: 'Shows stuff like the amount of servers I\'m  in, as well as my uptime and command count, and more stuff',
     category: "general",
     execute: async (client, config, Discord, target, utils, message, args) => {
-        var osu = require('node-os-utils')
-        var cpu = osu.cpu
- 
-        var count = cpu.count()
-        cpu.usage()
-        .then(cpuPercentage => {
 
         let totalSeconds = (client.uptime / 1000);
         let hours = Math.floor(totalSeconds / 3600);
@@ -17,8 +11,13 @@ module.exports = {
         let seconds = (totalSeconds % 60).toFixed(2)
 
         let uptime = `${hours}h ${minutes}min ${seconds}sec`;
-	
-	function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
+
+        var nonbotfarms = client.guilds.filter(s => s.members.filter(m => !m.user.bot).size < s.members.filter(m => m.user.bot).size).size
+        var hitomi = utils.fs.readFileSync("./util/random/hitomi.txt").toString().split("\n").length
+        var confessions = client.guilds.filter(c=>c.channels.find(e => e.name === "confessions") !== null).size
+        var snowball = client.guilds.filter(c=>c.roles.find(e=>e.name.toLowerCase() ==="snowball") !== null).size
+        
+        function formatBytes(a,b){if(0==a)return"0 Bytes";var c=1024,d=b||2,e=["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"],f=Math.floor(Math.log(a)/Math.log(c));return parseFloat((a/Math.pow(c,f)).toFixed(d))+" "+e[f]}
 
         var embed = new Discord.RichEmbed()
         .setColor('RANDOM')
@@ -27,22 +26,17 @@ module.exports = {
 Users: \`${client.users.size}\`
 Emojis: \`${client.emojis.size}\`
 Text channels: \`${client.channels.filter(c => c.type === "text").size}\`
-Non-bot farm servers (users > bots): \`${client.guilds.filter(s => s.members.filter(m => !m.user.bot).size < s.members.filter(m => m.user.bot).size).size}\`
-Uptime: \`${uptime}\`
-Commands: \`${client.commands.array().length}\`
-Number of prefixes: \`${require("../util/data holders/prefixes.json").length}\``, true)
-        .addField("Responses and other stuff", 
-`Pictures in **hi!hitomi:** \`${utils.fs.readFileSync("./util/random/hitomi.txt").toString().split("\n").length}\`
-Genders in **hi!gender:** \`${require("../util/random/genders.json").length}\`
-Sexualities in **hi!sexuality:** \`${require("../util/random/sexualities.json").length}\`
-Responses in **hi!ping**: \`${require("../util/random/ping.json").length}\` 
-Note that not all commands that respond with a random text aren't here
-`, true)
+Non-bot farm servers (users > bots): \`${nonbotfarms}\`
+Uptime: \`${uptime}\``, true)
+        .addField("Commands", 
+`Number of commands: \`${client.commands.size}\`
+Number of prefixes: \`${require("../util/data holders/prefixes.json").length}\`
+Pictures in **hi!hitomi:** \`${hitomi}\`
+Guilds with a **#confessions** channel: \`${confessions}\`
+Guilds with a **Snowball** role: \`${snowball}\``, true)
         .addField("System",
-`Memory usage: \`${formatBytes(process.memoryUsage().rss)}\`
-CPU usage: \`${cpuPercentage}%\``, true)
+`Memory usage: \`${formatBytes(process.memoryUsage().rss)}\``, true)
 message.channel.send(embed)
-        
-        })
+
     },
 }
