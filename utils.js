5b9ac0e958c29422fa3b94c2311763bdca70ecf5
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2019-09-20 20:27:58 +0200

    Big(?) update on checkCommand

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+var prefixes = require('./util/data holders/prefixes.json')
+var Discord = require("discord.js")
 module.exports = {
     //modules
     fs: require('fs'),
@@ -5,6 +7,12 @@ module.exports = {
     get: require("node-fetch"),
     eco: require("discord-economy"),
     settings: require("./util/data holders/guildsettings.json"),
+    weeb() {
+        const nekoclient = require('nekos.life');
+        const { sfw } = new nekoclient();
+
+        return sfw;
+    },
 
     //Other stuff
     racistRegex: /nig(g|)(a|er)|fag(|got|ger|git)|kys|trann(y|ies)/gi,
@@ -14,64 +22,57 @@ module.exports = {
     //functions
     unvaporwave(text) {
         try {
-        if (typeof text !== "string") { throw "(text) must be a string, nerd"}
-        var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789.?!,:;\"'-_+%=$*(){}[]<>|/~\\@&%£#"
-        letters = letters.split('')
-        var wp = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ　０１２３４５６７８９．？！，：；＂＇－＿＋％＝＄＊（）｛｝［］＜＞｜／～＼＠＆％￡＃"
-        wp = wp.split('')
-        text = text.split('')
-        for (i = 0; i < text.length; i++) {
-            if (wp.includes(text[i])) {
-                text[i] = letters[wp.indexOf(text[i])]
-            }
-        }
-        return text.join('')
+            if (typeof text !== "string") { throw "(text) must be a string, nerd" }
+            var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789.?!,:;\"'-_+%=$*(){}[]<>|/~\\@&%£#"
+                letters = letters.split('')
+            var wp = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ　０１２３４５６７８９．？！，：；＂＇－＿＋％＝＄＊（）｛｝［］＜＞｜／～＼＠＆％￡＃"
+                wp = wp.split('')
+            return text.split('').map(i => wp.includes(i) ? letters[wp.indexOf(i)] : i).join("")
         } catch (error) { console.log(error) }
-    }, 
+    },
 
     unemojify(text) {
-        return text.replace(/🅱/g, "b").replace(/🅾/g, "b").replace(/🅰/g, "b")
+        return text.replace(/🅱/g, "b").replace(/🅾/g, "o").replace(/🅰/g, "a")
     },
 
-    checkCommand(text, output) {
+    checkCommand(text) {
         try {
-        text = this.unemojify(text)
-        if (typeof text !== "string") {throw "Both text and output must be strings"}
-        var prefixes = require('./util/data holders/prefixes.json')
-        let prefix = false;
-        for (const thisPrefix of prefixes) {
-            if (this.unvaporwave(text.toLowerCase()).startsWith(thisPrefix)) prefix = thisPrefix;
-        }
-        if (this.unvaporwave(text.toLowerCase()).startsWith(prefix)) {
+            text = this.unvaporwave(text)
+
+
+            if (typeof text !== "string") { throw "Both text and output must be strings" }
+            let prefix = false;
+            for (const thisPrefix of prefixes) {
+                if (this.unemojify(text.toLowerCase()).startsWith(thisPrefix)) prefix = thisPrefix;
+            }
+
+            if (!prefix) return;
+
             var args = text.slice(prefix.length).trim().split(/ +/g);
-            args = this.unvaporwave(args.join(' ')).split(' ')
-            var commandName = args.shift().toLowerCase();
-            commandName = this.unvaporwave(commandName)
-            //this allows to use commands with full-width chars 
-	 
-            if (output === "prefix") {
-                return prefix;
-            } else
-            if (output === "name") {
-                return commandName;
-            } else 
-            if (output === "args") {
-                return args;
-            } else
-            if (output === "command") {
-                return prefix+commandName;
-            } else 
-            if (output === "multitarget") {
-                args.push("")
-                var victims = args.map(a => a.replace(/[^0-9]+/g, ""))
-	            victims = victims.slice(0, victims.indexOf(""))
-	            var reason = args.slice(victims.length, args.length)
-            return { "victims": [...new Set(victims)], "reason": reason}
-            } else return output;
-        } else return false
-    } catch (error) { console.log(error) }
+            var commandName = this.unemojify(args.shift().toLowerCase());
+
+            var victims = args.map(a => a.replace(/[^0-9]+/g, ""))
+            victims = victims.slice(0, victims.indexOf(""))
+            var reason = args.slice(victims.length, args.length).join(" ")
+
+            return {
+                args,
+                name: commandName, //will display the command name OR the alias used (if any)
+                command: prefix+commandName, //not only displays the command's name, but also the used prefix
+                victims: [...new Set(victims)],
+                reason
+            } 
+
+            /* 
+             * reason and victims are used for commands like ban or kick which accept multiple targets
+             * for example: hi!kick [user1] [user2] [reason]
+             * non number characters are removed from the targets to turn mentions into plain user IDs
+            */
+            
+        }
+        catch (error) { console.log(error) }
     },
-    
+
     setDefaultSettings(guildid) {
         this.settings[guildid] = this.settings["default"]
         this.fs.writeFile('./util/data holders/guildsettings.json', JSON.stringify(this.settings, null, 2), function (error) {
@@ -80,11 +81,10 @@ module.exports = {
             }
         })
     },
-  
+
     embedImage(zelda) {
-        const Discord = require("discord.js")
         var embed = new Discord.RichEmbed()
-            .setImage(zelda) 
+            .setImage(zelda)
         return embed
     },
 
@@ -93,9 +93,9 @@ module.exports = {
     },
 
     vaporwavecolor() {
-        var r = (Math.round(Math.random()* 127) + 127).toString(16);
-        var g = (Math.round(Math.random()* 127) + 127).toString(16);
-        var b = (Math.round(Math.random()* 127) + 127).toString(16);
+        var r = (Math.round(Math.random() * 127) + 127).toString(16);
+        var g = (Math.round(Math.random() * 127) + 127).toString(16);
+        var b = (Math.round(Math.random() * 127) + 127).toString(16);
         return '#' + r + g + b;
-    }     
+    }
 }
