1384ae1ba10ae734d4eae5697d767e71f69f0b83
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2020-05-10 14:54:16 +0200

    utils.multitarget update+user on ban no longer undefined

--- commands/ban.js
+++ commands/ban.js
@@ -6,34 +6,35 @@ module.exports = {
     category: "mod",
     execute: async (client, config, Discord, target, utils, message, args) => {
 
-	args.push("")
-
         if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Have you considered having ban perms first?")
         if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Hey, give me ban members perms!")
         
-        var victims = message.context.victims
-        var reason = message.context.reason
-        if (!reason[0]) { reason = `[${message.author.tag}] No reason apparently??????` } else { reason = `[${message.author.tag}] ${reason}` }
-        if (!victims[0]) return message.channel.send(`That's not how you use the command!
+        var arguments = utils.multitarget(args)
+        if (!arguments.targets[0]) return message.channel.send(`That's not how you use the command!
 \`\`\`hi!ban [user 1] [optional reason]
 hi!ban [user 1] [user 2] [etc...] [optional reason]\`\`\`
 Both mentions and users are valid. This command can also perform hackbanning (aka banning members that aren't on your server by using their user IDs)`)
 
-        var victimmsg = []
-        await victims.forEach(async e => {
-            var ded = message.guild.ban(e, reason).then(async ded => {
-                var dedname = `**${ded.tag} (${ded.id})**`
+
+        await arguments.targets.forEach(async e => {
+            var ded = message.guild.ban(e, `[${message.author.tag}] ${arguments.reason}`).then(async ded => {
+
+                if (!client.users.has(e)) { await client.fetchUser(e) 
+                    ded = client.users.get(e)
+                } //fetch the user if hitomi doesn't have them in their cache, so banname doesn't become "undefined (undefined)" 
+                
+                var banname = `**${ded.tag} (${ded.id})**`
                 message.channel.send("Succesfully " +
                     utils.random([
-                        `BEANED ${dedname}!!!! <:Beaned:468493110851141633>`,
-                        `destroyed ${dedname} from existence.`,
-                        `~~unbanned~~ ${dedname} <:troll:381546270528897024>`,
-                        `bent ${dedname}. 🔨`,
-                        `bAnNeD *${dedname},`,
-                        `deleted ${dedname}. <:WindowsRecycleBin:389117071922954240>`,
-                        `killed ${dedname}. 💀`,
-                        `owned ${dedname}.`,
-                        `sent ${dedname} to hell.`
+                        `BEANED ${banname}!!!! <:Beaned:468493110851141633>`,
+                        `destroyed ${banname} from existence.`,
+                        `~~unbanned~~ ${banname} <:troll:381546270528897024>`,
+                        `bent ${banname}. 🔨`,
+                        `bAnNeD *${banname},`,
+                        `deleted ${banname}. <:WindowsRecycleBin:389117071922954240>`,
+                        `killed ${banname}. 💀`,
+                        `owned ${banname}.`,
+                        `sent ${banname} to hell.`
                     ])()
                 )
             }).catch(ee => message.channel.send(`Failed to ban **${client.users.get(e).tag}**!!!!!!!!!!! :joy:`))
