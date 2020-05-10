e05860371b57220a477db6c2ee99f40dd9d33c52
Tina the Cyclops <30502402+GmdDjca@users.noreply.github.com>
2020-05-10 17:04:32 +0200

    Now you can use this with bots hitomi hasn't cached

--- commands/genoauth.js
+++ commands/genoauth.js
@@ -8,16 +8,17 @@ module.exports = {
             message.channel.send(`Please specify a bot (by mention or ID)`)
         } else{
             if (client.users.get(args[0].replace(/[^0-9]/g, "")) == undefined && target.id === message.author.id) {
-                target = client.fetchUser(args[0].replace(/[^0-9]/g, ""))
+                await client.fetchUser(args[0].replace(/[^0-9]/g, ""))
+                target = client.users.get(args[0])
             }
             if (!target.bot || target == undefined) {
                 message.channel.send("That's not a bot <:LMFAO:443921300201799681>")
             }  else {
                 message.channel.send("Look in your DM's :heart:")
         message.author.send(`Have fun with \`${target.tag}\`!!
-https://discordapp.com/oauth2/authorize?client_id=${target.id}&scope=bot&permissions=2146958591
+<https://discordapp.com/oauth2/authorize?client_id=${target.id}&scope=bot&permissions=2146958591>
 **disclaimer: this link is NOT guaranteed to work. Some bots are private or need a grant code, for example**
-\`tip: if you don't want the bot to create it's default role, uncheck all permissons before inviting it\``)
+\`tip: if you don't want the bot to create it's default role, uncheck every permission before inviting it\``)
 }}
     },
 }
